79a963502ab7133a1df10e76c758c7e873f942fd
lijiale <2583390175@qq.com>
2024-08-17 20:36:10 +0800

    完成1.3，优化第一个界面的提示按钮

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,22 +18,29 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-       //第一个按钮：提示，其java逻辑实现
-        Button buttonShowMessage = findViewById(R.id.btnOne);
-        final TextView textViewMessage = findViewById(R.id.txtTwo);
-        buttonShowMessage.setOnClickListener(new View.OnClickListener() {
+       //获取提示的Button控件，通过其ID引用它，按一下出现提示，再按一下提示消失
+        Button btnOne = findViewById(R.id.btnOne);
+        TextView txtTwo = findViewById(R.id.txtTwo);
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            boolean isShow = false;//做一个提示按钮标志，实现点击出现和点击消失
             @Override
             public void onClick(View v) {
-                textViewMessage.setVisibility(View.VISIBLE);
+                if(isShow){
+                    txtTwo.setVisibility(View.GONE);
+                    isShow = false;
+                }else{
+                    txtTwo.setVisibility(View.VISIBLE);
+                    isShow = true;
+                }
             }
         });
 
-        //第二个按钮：点击跳转下一个界面，其java逻辑实现
-        Button buttonNavigate = findViewById(R.id.btnTwo);
-        buttonNavigate.setOnClickListener(new View.OnClickListener() {
+        //获取跳转界面的Button控件，通过其ID引用它，按一下跳转到第二个布局界面
+        Button btnTwo = findViewById(R.id.btnTwo);
+        btnTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 创建 Intent 以启动 SecondActivity
+                // 创建 Intent 以启动第二个界面 MainActivity2
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
             }
